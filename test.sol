99ed3d8e25e5f8d90e2c139fc6ac398746378456
Denys Proskura <57964380+MrJeleika@users.noreply.github.com>
2026-04-26 18:11:34 +0200

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,1 +1,35 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract TipJar {
+    address public owner;
+    uint256 public totalTips;
+
+    event TipReceived(address sender, uint256 amount);
+    event Withdrawn(address owner, uint256 amount);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function tip() public payable {
+        require(msg.value > 0, "Must send ETH");
+        totalTips += msg.value;
+        emit TipReceived(msg.sender, msg.value);
+    }
+
+    function withdraw() public {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds to withdraw");
+
+        (bool success, ) = payable(owner).call{value: balance}("");
+        require(success, "Transfer failed");
+
+        emit Withdrawn(owner, balance);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
